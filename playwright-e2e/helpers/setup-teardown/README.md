e576eedbf3c79b6a02791fe6b17553301c3d7f9f
abigailflorence97 <abigail.smith@hmcts.net>
2024-09-30 14:52:32 +0100

    setup/teardown structure

---
+++ playwright-e2e/helpers/setup-teardown/README.md
@@ -0,0 +1,3 @@
+# Playwright Global setup and tear down
+
+Following playwright doc: https://playwright.dev/docs/test-global-setup-teardown
---
+++ playwright-e2e/helpers/setup-teardown/global.setup.ts
@@ -0,0 +1,10 @@
+import { test as setup } from '@playwright/test';
+
+setup('create new database', async ({ }) => {
+  console.log('creating new user');
+  // create user
+
+  console.log('creating case')
+  // create case
+});
+
